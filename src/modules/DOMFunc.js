2bc63d294034279d43cd869b47962982a1485320
dulnithLiyanage <dulnithliyanage2@gmail.com>
2022-04-05 21:44:56 +0530

    Added sidebar functionality

--- src/modules/DOMFunc.js
+++ src/modules/DOMFunc.js
@@ -1,8 +1,28 @@
 import {
   eventOpenButton,
-  eventCloseButton,
-  eventAddTask,
+  eventCloseModal,
+  eventOpenSidebar,
+  eventCloseSidebar,
+  eventCreateProject,
+  eventRenderCollection,
+  eventInitalizeCollections,
 } from "./eventListeners";
+
+export const initializeDefaultCollections = () => {
+  const collections = document.querySelectorAll(".collection");
+
+  collections.forEach((collection) => {
+    eventInitalizeCollections(collection);
+  });
+};
+
+export const renderCollectionOnClick = () => {
+  const collections = document.querySelectorAll(".collection");
+  console.log(collections);
+  collections.forEach((collection) => {
+    eventRenderCollection(collection);
+  });
+};
 
 export const openModalOnClick = () => {
   const openModalButton = document.querySelector("#open-modal");
@@ -15,11 +35,25 @@ export const closeModalOnClick = () => {
   const closeModalButton = document.querySelector("#close-modal");
   const modal = document.querySelector("#modal");
 
-  eventCloseButton(closeModalButton, modal);
+  eventCloseModal(closeModalButton, modal);
 };
 
-export const createTaskOnClick = () => {
-  const form = document.querySelector("#form");
+export const openSidebarOnClick = () => {
+  const openSidebarButton = document.querySelector("#open-sidebar");
+  const sidebar = document.querySelector(".sidebar");
 
-  eventAddTask(form);
+  eventOpenSidebar(openSidebarButton, sidebar);
+};
+
+export const closeSidebarOnClick = () => {
+  const closeSidebarButton = document.querySelector("#close-sidebar");
+  const sidebar = document.querySelector(".sidebar");
+
+  eventCloseSidebar(closeSidebarButton, sidebar);
+};
+
+export const createProjectOnClick = () => {
+  const addProject = document.querySelector(".add-project");
+
+  eventCreateProject(addProject);
 };
