9680d79fdef1b986d2d1c0b6f5fdcfbde076d039
dulnithLiyanage <dulnithliyanage2@gmail.com>
2022-04-07 13:02:15 +0530

    Added more functionality to the ToDo Component

--- src/modules/components.js
+++ src/modules/components.js
@@ -1,16 +1,41 @@
 export const ListOfCollections = [];
 
-export const ToDo = (title, description, dueDate, priority) => {
+export const ToDo = (title, description, dueDate) => {
+  let priority = false;
+  let completed = false;
+
+  const prioritize = () => {
+    priority = true;
+  };
+
+  const unPrioritize = () => {
+    priority = false;
+  };
+
+  const complete = () => {
+    completed = true;
+  };
+
+  const undoComplete = () => {
+    completed = false;
+  };
+
   const getTitle = () => title;
   const getDescription = () => description;
   const getDueDate = () => dueDate;
-  const getPriority = () => priority;
+  const checkIfImportant = () => priority;
+  const checkIfCompleted = () => completed;
 
   return {
     getTitle,
     getDescription,
     getDueDate,
-    getPriority,
+    checkIfImportant,
+    checkIfCompleted,
+    prioritize,
+    unPrioritize,
+    complete,
+    undoComplete,
   };
 };
 
@@ -19,11 +44,16 @@ export const Collection = (collectionName, tasks) => {
     tasks.push(task);
   };
 
+  const removeTask = (task) => {
+    tasks = tasks.filter((todo) => todo !== task);
+  };
+
   const getCollectionName = () => collectionName;
   const getTasks = () => tasks;
 
   return {
     addTask,
+    removeTask,
     getCollectionName,
     getTasks,
   };
